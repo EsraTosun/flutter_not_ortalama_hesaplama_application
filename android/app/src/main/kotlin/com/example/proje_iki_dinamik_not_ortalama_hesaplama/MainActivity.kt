e84fea29d337eee3afb300ebbfe4b70546fdf53f
EsraTosun <esratosun171@gmail.com>
2023-07-25 22:30:51 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/proje_iki_dinamik_not_ortalama_hesaplama/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.proje_iki_dinamik_not_ortalama_hesaplama
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:proje_iki_dinamik_not_ortalama_hesaplama/constants/app_constants.dart';
+import 'package:proje_iki_dinamik_not_ortalama_hesaplama/widgets/ortalama_hesapla_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(   //Uygulamanın ana teması
+      debugShowCheckedModeBanner: false,
+      title: 'Dinamik Ortalama Hesapla',
+      theme: ThemeData(
+        primarySwatch: Sabitler.anaRenk,     //Tanımladığımız renge uygun renkleride tanımlar
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        //Flutter farklı ortamlarda da çalıştığı için o ortama uygun görünmesini sağlar
+      ),
+      home: OrtalamaHesaplaPage(),
+    );
+  }
+}
+
